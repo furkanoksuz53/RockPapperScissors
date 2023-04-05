e68a1062d51cf45746c0601a5f7efea88f18206f
furkanoksuz53 <80399254+furkanoksuz53@users.noreply.github.com>
2023-04-05 15:41:53 +0300

    Add files via upload

---
+++ RockPapperScissors/RockPapperScissors/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace RockPapperScissors
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numMaxScore = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxPlayer1Name = new System.Windows.Forms.TextBox();
+            this.tbxPlayer2Name = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblChoice2 = new System.Windows.Forms.Label();
+            this.lblChoice1 = new System.Windows.Forms.Label();
+            this.lblPlayer2Score = new System.Windows.Forms.Label();
+            this.lblPlayer1Score = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblVersion = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxScore)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numMaxScore
+            // 
+            this.numMaxScore.Location = new System.Drawing.Point(135, 7);
+            this.numMaxScore.Name = "numMaxScore";
+            this.numMaxScore.Size = new System.Drawing.Size(150, 27);
+            this.numMaxScore.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oyuncu1 Adı";
+            // 
+            // tbxPlayer1Name
+            // 
+            this.tbxPlayer1Name.Location = new System.Drawing.Point(14, 71);
+            this.tbxPlayer1Name.Name = "tbxPlayer1Name";
+            this.tbxPlayer1Name.Size = new System.Drawing.Size(125, 27);
+            this.tbxPlayer1Name.TabIndex = 2;
+            // 
+            // tbxPlayer2Name
+            // 
+            this.tbxPlayer2Name.Location = new System.Drawing.Point(186, 71);
+            this.tbxPlayer2Name.Name = "tbxPlayer2Name";
+            this.tbxPlayer2Name.Size = new System.Drawing.Size(125, 27);
+            this.tbxPlayer2Name.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kaçta Bitsin?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Oyuncu2 Adı";
+            // 
+            // lblChoice2
+            // 
+            this.lblChoice2.AutoSize = true;
+            this.lblChoice2.Location = new System.Drawing.Point(202, 133);
+            this.lblChoice2.Name = "lblChoice2";
+            this.lblChoice2.Size = new System.Drawing.Size(13, 20);
+            this.lblChoice2.TabIndex = 7;
+            this.lblChoice2.Text = " ";
+            // 
+            // lblChoice1
+            // 
+            this.lblChoice1.AutoSize = true;
+            this.lblChoice1.Location = new System.Drawing.Point(26, 133);
+            this.lblChoice1.Name = "lblChoice1";
+            this.lblChoice1.Size = new System.Drawing.Size(13, 20);
+            this.lblChoice1.TabIndex = 6;
+            this.lblChoice1.Text = " ";
+            // 
+            // lblPlayer2Score
+            // 
+            this.lblPlayer2Score.AutoSize = true;
+            this.lblPlayer2Score.Location = new System.Drawing.Point(202, 101);
+            this.lblPlayer2Score.Name = "lblPlayer2Score";
+            this.lblPlayer2Score.Size = new System.Drawing.Size(17, 20);
+            this.lblPlayer2Score.TabIndex = 9;
+            this.lblPlayer2Score.Text = "0";
+            // 
+            // lblPlayer1Score
+            // 
+            this.lblPlayer1Score.AutoSize = true;
+            this.lblPlayer1Score.Location = new System.Drawing.Point(26, 101);
+            this.lblPlayer1Score.Name = "lblPlayer1Score";
+            this.lblPlayer1Score.Size = new System.Drawing.Size(17, 20);
+            this.lblPlayer1Score.TabIndex = 8;
+            this.lblPlayer1Score.Text = "0";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 167);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(143, 56);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Başla";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(168, 167);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(143, 56);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(317, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(304, 224);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(11, 250);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(50, 20);
+            this.lblVersion.TabIndex = 13;
+            this.lblVersion.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 282);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblPlayer2Score);
+            this.Controls.Add(this.lblPlayer1Score);
+            this.Controls.Add(this.lblChoice2);
+            this.Controls.Add(this.lblChoice1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxPlayer2Name);
+            this.Controls.Add(this.tbxPlayer1Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMaxScore);
+            this.Name = "Form1";
+            this.Text = "RockPapperScissors";
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxScore)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private NumericUpDown numMaxScore;
+        private Label label1;
+        private TextBox tbxPlayer1Name;
+        private TextBox tbxPlayer2Name;
+        private Label label2;
+        private Label label3;
+        private Label lblChoice2;
+        private Label lblChoice1;
+        private Label lblPlayer2Score;
+        private Label lblPlayer1Score;
+        private Button btnStart;
+        private Button btnClear;
+        private DataGridView dataGridView1;
+        private Label lblVersion;
+    }
+}
---
+++ RockPapperScissors/RockPapperScissors/Form1.cs
@@ -0,0 +1,171 @@
+﻿namespace RockPapperScissors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lblVersion.Text = "v1.0";
+            Clear();       
+
+        }
+
+        int playerCount=2;
+        
+
+        List<Player> players = new List<Player>();
+        List<Choice> choices = new List<Choice>();
+        List<Result> results = new List<Result>();
+
+
+        private void GetChoice()
+        {
+            Choice rock = new Choice()
+            {
+                Name = "taş",
+                Id = 1
+            };
+            Choice papper = new Choice()
+            {
+                Name = "kağıt",
+                Id = 2
+            };
+            Choice scissors = new Choice()
+            {
+                Name = "makas",
+                Id = 3
+            };
+            choices.Add(rock);
+            choices.Add(papper);
+            choices.Add(scissors);
+            
+
+            Player player1 = new Player() 
+            { 
+                Name=tbxPlayer1Name.Text,
+                Score=0,
+                ChoiceId=0
+            };
+            Player player2 = new Player()
+            {
+                Name = tbxPlayer2Name.Text,
+                Score = 0,
+                ChoiceId = 0
+            };
+            players.Add(player1);
+            players.Add(player2);
+            GetResult();
+        }       
+        private void GetResult()
+        {
+            lblPlayer1Score.Text = players[0].Score.ToString();
+            lblPlayer2Score.Text = players[1].Score.ToString();
+
+            Random rnd = new Random();
+            players[0].ChoiceId = rnd.Next(1, 4);
+            players[1].ChoiceId = rnd.Next(1, 4);
+
+            Choice choice1 = choices.Where(c => c.Id == players[0].ChoiceId).First();
+            Choice choice2 = choices.Where(c => c.Id == players[1].ChoiceId).First();
+
+            lblChoice1.Text = choice1.Name;
+            lblChoice2.Text = choice2.Name;
+
+            Result result = new Result() 
+            {
+                Choice1= choice1.Name,
+                Choice2= choice2.Name
+            };
+
+            results.Add(result);
+
+
+
+            if (players[0].ChoiceId - players[1].ChoiceId==1 || players[0].ChoiceId - players[1].ChoiceId == -2)
+            {
+                players[1].Score += 0;
+                players[0].Score += 1;
+                lblPlayer1Score.Text = players[0].Score.ToString();
+                lblPlayer2Score.Text = players[1].Score.ToString();
+                CheckScore(players[0]);
+            }
+            else if(players[0].ChoiceId - players[1].ChoiceId == 0)
+            {
+                players[0].Score += 0;
+                players[1].Score += 0;
+                CheckScore(players[0]);
+
+            }
+            else
+            {
+                players[0].Score += 0;
+                players[1].Score += 1;
+                lblPlayer1Score.Text = players[0].Score.ToString();
+                lblPlayer2Score.Text = players[1].Score.ToString();
+                CheckScore(players[1]);
+            }
+
+        }
+        private void CheckScore(Player player)
+        {
+            string maxScore = numMaxScore.Value.ToString();
+
+            if(player.Score==Convert.ToUInt32(maxScore))
+            {
+                MessageBox.Show($"{player.Name} kazandı ");
+                dataGridView1.DataSource = results;
+            }
+            else
+            {
+                GetChoice();
+            }
+        }
+        private void Clear()
+        {
+            foreach(Player player in players)
+            {
+                player.Score = 0;
+                player.ChoiceId = 0;
+            }
+            tbxPlayer1Name.Text = string.Empty;
+            tbxPlayer2Name.Text = string.Empty;
+            lblPlayer1Score.Text = "0";
+            lblPlayer2Score.Text = "0";
+            numMaxScore.Value = 0;
+            dataGridView1.DataSource = null;
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GetChoice();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public int ChoiceId { get; set; }
+    }
+    public class Choice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Result
+    {
+        public string Choice1 { get; set; }
+        public string Choice2 { get; set; }
+
+    }
+}
